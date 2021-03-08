e61aeb6e5f92565f746e1174941ed200b705e174
Simran Farheen <simranf996@gmail.com>
2021-03-09 03:44:42 +0530

    Completed Part C of the code and added the option to view/edit a single post page and add comments as well as likes

--- scripts/postlist.js
+++ scripts/postlist.js
@@ -2,13 +2,29 @@
 var deletePostModal = document.getElementById("deleteModal");
 
 // Get the button that opens the modal
-var deletePostButton = document.getElementById("deleteButton");
+var deletePostButton1 = document.getElementById("deleteButton1");
+var deletePostButton2 = document.getElementById("deleteButton2");
+var deletePostButton3 = document.getElementById("deleteButton3");
+var deletePostButton4 = document.getElementById("deleteButton4");
+var deletePostButton5 = document.getElementById("deleteButton5");
 
 // Get the <span> element that closes the modal
 var deletePostClose = document.getElementsByClassName("delete-close")[0];
 
 // When the user clicks on the button, open the modal
-deletePostButton.onclick = function() {
+deletePostButton1.onclick = function() {
+  deletePostModal.style.display = "block";
+}
+deletePostButton2.onclick = function() {
+  deletePostModal.style.display = "block";
+}
+deletePostButton3.onclick = function() {
+  deletePostModal.style.display = "block";
+}
+deletePostButton4.onclick = function() {
+  deletePostModal.style.display = "block";
+}
+deletePostButton5.onclick = function() {
   deletePostModal.style.display = "block";
 }
 
